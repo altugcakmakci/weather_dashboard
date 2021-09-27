5db90c78342998034afa0c075fc85b2e1c3fe422
Altug Cakmakci <altugcakmakci@gmail.com>
2021-09-27 19:11:38 -0400

    check city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
 let cityButton = document.getElementById("city-button");
 
 function populateCity() {
-    addNewCity();
+    let cityName = document.getElementById("cityName").value.trim();
+    getCityLocation(cityName);
+
 }
 
-function addNewCity(){
+function addNewCity(cityName){
     console.log("Here");
+    
     let cityList = document.getElementById("city-list");
     let newCityEl = document.createElement("li");
     newCityEl.className = "list-group-item list-group-item-primary m-1";
-    console.log(document.getElementById("cityName").value.trim());
-    newCityEl.textContent = document.getElementById("cityName").value.trim();
-    newCityEl.setAttribute("data-attribute",document.getElementById("cityName").value.trim());
+    
+    console.log(cityName);
+    newCityEl.textContent = cityName;
+    newCityEl.setAttribute("data-attribute",cityName);
     newCityEl.addEventListener("click", function (event) {
         event.preventDefault();
         let targetCity = event.target.getAttribute("data-attribute");
@@ -19,6 +23,28 @@ function addNewCity(){
     });
     cityList.appendChild(newCityEl);
     document.getElementById("cityName").value="";
+    
 }
 
 cityButton.addEventListener("click",populateCity);
+
+let getCityLocation = function (cityName) {
+    
+    let apiUrl = 'http://api.weatherapi.com/v1/forecast.json?key=06e270c630a944389ef150954212709&q='+cityName+'&aqi=no';
+    console.log(apiUrl);
+    fetch(apiUrl).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+            console.log(data);
+            console.log(data.location);
+            let region = data.location.region;
+            let cityTitleEl = document.getElementById("city-title");
+            cityTitleEl.textContent = cityName + ", " + region + " ("+moment().format("MMMM do,YYYY")+")";
+            addNewCity(cityName);
+        });
+      } else {
+        alert("Please enter a valid city name!");
+        document.getElementById("cityName").value="";
+      }
+    });
+  };
